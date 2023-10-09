c04cf192f812e12019d38789b9f4fa6818e7f089
Andrei Koshel <ciceronn21@gmail.com>
2023-10-09 19:45:51 +0300

    добавил метод get() в ice_cream/views.py

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
+from ice_cream.models import IceCream
 
 
 def ice_cream_detail(request, pk):
-    template = 'ice_cream/detail.html'
-    context = {}
-    return render(request, template, context)
+    template_name = "ice_cream/detail.html"
+    ice_cream = IceCream.objects.get(pk=pk)
+    context = {
+        "ice_cream": ice_cream,
+    }
+    return render(request, template_name, context)
 
 
 def ice_cream_list(request):
-    template = 'ice_cream/list.html'
+    template = "ice_cream/list.html"
     context = {}
     return render(request, template, context)
