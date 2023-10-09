0be4d854ba6eb47dcbc3621d142432fb3151cf1d
Andrei Koshel <ciceronn21@gmail.com>
2023-10-09 20:01:48 +0300

    На главную выведены категории для каждого вида мороженого

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -5,13 +5,9 @@ from ice_cream.models import IceCream
 
 def index(request):
     template_name = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values(
-            'id', 'title', 'description'
-        ).filter(
-        Q(is_on_main=True)
-        & Q(is_published=True)
-        | Q(title__contains='пломбир')
-        & Q(is_published=True))
+
+    ice_cream_list = IceCream.objects.all()
+
     context = {
         'ice_cream_list': ice_cream_list,
     }
